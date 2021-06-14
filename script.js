c2b993e0b8b8cfee553fe35c84a6576e6df99dc4
Katrine Collins <katrine.collins1@gmail.com>
2021-06-13 20:16:18 -0400

    password generator working

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 
-// 1. handle button click, you need to create a click event using the addEventListener for this generate button
-
-// 2. display - window.prompt() to ask the user for size of the password
-// 3. validate - only numbers and size of password, the range  >8 and <128
-// 4. prompt- window.confirm()  to ask user for ok or cancel for the type of password
-// 5. You need 4 confirms, each confirm represents the type of character in the password
-
 var generatePassword = function () {
-  var finalPassword = "aw" //finalPassword.length
+  var finalPassword = "" 
   var lowercase = "abcdefghijklmnopqrstuvwxyz"
   var uppercase = "ABCDEFGHIJKLONOPQRSTUVWXYZ"
   var specialCharcters = "%$#()"
@@ -18,49 +11,76 @@ var generatePassword = function () {
   console.log(passwordsize)
 
   var isLowerCase = confirm("Does your password include lowercase?")
+
+  console.log(isLowerCase) 
   
   var isUpperCase = confirm("Does your password include Uppercase?")
+
+  console.log(isUpperCase)
   
   var isSpecialCharacter= confirm("Does your password include  special?")
+
+  console.log(isSpecialCharacter)
   
   var isNumber = confirm("Does your password include number?")
-
-  console.log(isLowerCase)
+  
+  console.log(isNumber)
 
   // ending 
 
-  if (isLowerCase) {
+  for (let i = 0; i < passwordsize; i++) {
 
+   //  && AND  --> if at least one of the statements is false, then the final outcome is false
+   //  || OR   --> if at least one of the statements is true, then the final outcome is true
+   //  ! NOT
 
-    var randomPickLowerCase = Math.floor(Math.random() * lowercase.length)
-
-    var pickOneLowerCaseLetter = lowercase.charAt(randomPickLowerCase)
-    finalPassword =finalPassword+ pickOneLowerCaseLetter 
+      if (isLowerCase && finalPassword.length < passwordsize) {
+        // var lowercase = "abcdefghijklmnopqrstuvwxyz"
+        //                  012345
+    
+        var randomPickLowerCase = Math.floor(Math.random() * lowercase.length)
+        //  randomPickLowerCase = 15
+    
+        var pickOneLowerCaseLetter = lowercase.charAt(randomPickLowerCase)
+        // pickOneLowerCaseLetter =  "abcdefghijklmnopqrstuvwxyz".charAt(15)
+        // pickOneLowerCaseLetter =  "p"
+    
+        finalPassword =finalPassword+ pickOneLowerCaseLetter 
+        // finalPassword =  "" + "p"
+        // finalPassword = "p"
+      }
+    
+    
+      if(isUpperCase && finalPassword.length < passwordsize){
+        var randomPickUpperCase = Math.floor(Math.random() * uppercase.length)
+        // 2
+    
+        var pickOneUpperCaseLetter = uppercase.charAt(randomPickUpperCase)
+        // C
+    
+        finalPassword =finalPassword+ pickOneUpperCaseLetter
+        // finalPassword = "p" + "C"
+        // finalPassword= "pC"
+      }
+    
+    
+      if(isSpecialCharacter  && finalPassword.length < passwordsize){
+        var randomPickSpecial = Math.floor(Math.random() * specialCharcters.length)
+        var pickOneSpecialCharacterLetter = specialCharcters.charAt(randomPickSpecial)
+        finalPassword =finalPassword+ pickOneSpecialCharacterLetter
+      }
+    
+    
+      if(isNumber && finalPassword.length < passwordsize){
+        var randomPickNumber = Math.floor(Math.random() * number.length)
+        var pickOneNumber = number.charAt(randomPickNumber)
+        finalPassword =finalPassword+  pickOneNumber
+      }
+    
   }
 
 
-  if(isUpperCase){
-    var randomPickUpperCase = Math.floor(Math.random() * uppercase.length)
-    var pickOneUpperCaseLetter = uppercase.charAt(randomPickUpperCase)
-    finalPassword =finalPassword+ pickOneUpperCaseLetter
-  }
-
-
-  if(isSpecialCharacter){
-    var randomPickSpecial = Math.floor(Math.random() * specialCharcters.length)
-    var pickOneSpecialCharacterLetter = specialCharcters.charAt(randomPickSpecial)
-    finalPassword =finalPassword+ pickOneSpecialCharacterLetter
-  }
-
-
-  if(isNumber){
-    var randomPickNumber = Math.floor(Math.random() * number.length)
-    var pickOneNumber = number.charAt(randomPickNumber)
-    finalPassword =finalPassword+  pickOneNumber
-  }
-
-
-  console.log(pickOneLowerCaseLetter, pickOneUpperCaseLetter, pickOneSpecialCharacterLetter, pickOneNumber)
+  console.log(finalPassword)
 
 
 
